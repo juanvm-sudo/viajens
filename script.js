da2acc8bb462f7bc58a775a1fe7034c51a9f7147
juanvm-sudo <juan.caceres.martins@escola.pr.gov.br>
2026-03-23 17:11:15 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ const avanca = document.querySelectorAll('.btn-proximo');
 
 avanca.forEach(button => {
     button.addEventListener('click', function(){
-        const atual = document.querySelector('.ativo')
-        const proximoPasso = 'passo-' + this.getAttribute('data-proximo')
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
 
-        atual.classList.remove('ativo')
-        document.getElementById(proximoPasso).classList.add('ativo')
-    })
-})
+        if (document.getElementById(proximoPasso)) {
+            atual.classList.remove('ativo');
+            document.getElementById(proximoPasso).classList.add('ativo');
+        }
+    });
+});
